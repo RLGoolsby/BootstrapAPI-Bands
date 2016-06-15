d68f23e9eb5d1cf14a08b347439b644b7a8dc691
Rhonda Goolsby <rlgoolsby@gmail.com>
2016-06-15 09:51:41 -0400

    Add extra ideas

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,19 +2,20 @@
 //  THIS WAS MY SECOND PLAN--MY INTENTION WAS TO CONNECT BAND NAMES AND LYRICS TO BABY IMAGES AND NAMES
 //  TO THE BANDS/SONGS THAT CONTAIN THE WORD BABY IN THEM.     THIS ALL PRESENTED ITSELF TO  AS
 //  I CONTINUED TO TRY TO FIND A GOOD API AND TRY TO TIE IT ALL TOGETHER.   ALAS.  I NEED MORE TIME
-//  TO REALLY MAKE IT WORK. 
+//  TO REALLY MAKE IT WORK.
 
 
 //////////////////////////////////////////////////////////////////////////////////
 // (function(){
 //   "use strict";
 ////////   API CONNNECT FOR BAND NAMES                      ////////////////////
-var artistname = $('input[name="artist"]').val();
+var artistname = $('input[name="artist"]').val(); + artist + "/" + baby + "/" + //"type=track&limit=10",  extra ideas to remember from classmates
 // var weathertype =$('input[type="weather"]').val();
 
 $(".form-control").on("click", function(){
-$.getJSON('http://api.bandsintown.com/Fleetwood-Mac/.json?api_version=2.0&app_id=rhonda')
+$.getJSON('http://api.bandsintown.com/Fleetwood-Mac/.json?api_version=2.0&app_id=rhonda' + artist + "/" + baby + "/" + //"type=track&limit=10",)  extra ideas
 .done(success);
+
 
 function success () {
   $('input-group-btn').append();
